b9003219ba75914a7eb9e8e533c4a8429e504442
gatesma <gatesma@foxmail.com>
2020-05-09 20:20:28 +0800

    web

---
+++ src/main/java/com/gatesma/webex/service/UserService.java
@@ -0,0 +1,12 @@
+package com.gatesma.webex.service;
+
+/**
+ * Copyright (C), 2020
+ * FileName: UserService
+ * Author:   Marlon
+ * Email: gatesma@foxmail.com
+ * Date:     2020/5/9 20:20
+ * Description:
+ */
+public interface UserService {
+}
